0e8dbf2cf29a0d6223c9e681ced452700056b4db
Arturo <arturo.veras@zeke.cl>
2017-07-12 13:55:04 -0400

    first add

---
+++ serial_server.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+from time import gmtime, strftime
+from datetime import datetime
+import time
+import serial
+ser = serial.Serial('/dev/ttyUSB0', 500000);
+
+fname = 'sensor/100hz_ADXL345_v22.csv'
+fmode = 'ab'
+bufferIn = "";
+with open(fname, fmode) as outf:
+    try:
+        while True:
+            # Receive the data one byte at a time
+            data = ser.readline();
+            bufferIn = "%s%s" % (bufferIn, data)
+            utcTime = strftime("%d-%m-%y %H:%M:%S", gmtime()) + "." + str(datetime.now().microsecond) + ","
+
+            if '\n' in data:
+            	outf.write(bufferIn[4:])
+            	print "received data:", bufferIn
+            	bufferIn = ""
+    except KeyboardInterrupt:
+        print('exiting.')
+
+
