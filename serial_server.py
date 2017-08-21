6b43570833faf04c3afdd9dd77e0b778015f530e
Arturo <arturo.veras@zeke.cl>
2017-08-21 18:02:27 -0300

    mejoras

--- serial_server.py
+++ serial_server.py
@@ -4,12 +4,26 @@ from time import gmtime, strftime
 from datetime import datetime
 import time
 import serial
+import matplotlib.pyplot as plt
+import time, os, fnmatch, shutil
 
 
+t = time.localtime()
+timestamp = time.strftime('%b-%d-%Y_%H%M', t)
+#BACKUP_NAME = ("./data/CA1393BA-" + timestamp + ".csv")
+#ser = serial.Serial('/dev/ttyUSB1', 500000);
 
+#BACKUP_NAME = ("./data/CA2117SB-" + timestamp + ".csv")
+#ser = serial.Serial('/dev/ttyUSB2', 500000);
+
+BACKUP_NAME = ("./data/CC4615BA-" + timestamp +".csv")
 ser = serial.Serial('/dev/ttyUSB0', 500000);
 
-fname = 'sensor/CA1393BA_4_8_2017_6.csv'
+
+#BACKUP_NAME = ("./data/ST1679BA-" + timestamp +".csv")
+#ser = serial.Serial('/dev/ttyUSB3', 500000);
+
+fname = BACKUP_NAME
 fmode = 'ab'
 bufferIn = "";
 with open(fname, fmode) as outf:
@@ -17,7 +31,7 @@ with open(fname, fmode) as outf:
         while True:
             # Receive the data one byte at a time
             data = ser.readline();
-            print data
+            print (data)
             outf.write(data)
     except KeyboardInterrupt:
         print('exiting.')
